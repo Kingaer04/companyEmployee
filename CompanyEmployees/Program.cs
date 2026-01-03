7e76be9449623cbac63e85ab6bbe24cef1252c7b
kingaer04 <danielanifowoshe04@gmail.com>
2026-01-03 16:57:17 +0100

    setting up logger service using (nlog)

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,33 @@
+using CompanyEmployees.Extensions;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
