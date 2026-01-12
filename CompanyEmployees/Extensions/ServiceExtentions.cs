9a05d20562cb70a741c333a378cf2e99820adb4a
kingaer04 <danielanifowoshe04@gmail.com>
2026-01-12 17:51:31 +0100

    Added the service layer architecture to the project

--- CompanyEmployees/Extensions/ServiceExtentions.cs
+++ CompanyEmployees/Extensions/ServiceExtentions.cs
@@ -1,5 +1,7 @@
 ﻿using Contracts;
 using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Repository;
 
 namespace CompanyEmployees.Extensions
 {
@@ -13,14 +15,20 @@ namespace CompanyEmployees.Extensions
                 .AllowAnyMethod()
                 .AllowAnyHeader());
             });
-
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
             services.Configure<IISOptions>(options =>
             {
 
             });
-
         public static void ConfigureLoggerService(this IServiceCollection services) =>
             services.AddSingleton<ILoggerManager, LoggerManager>();
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+            services.AddScoped<IServiceManager, ServiceManager>();
+        public static void ConfigureSqlContext(this IServiceCollection services,
+        IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
     }
 }
