4f67feb719b11af81afb5f2a991adf022a101024
kingaer04 <danielanifowoshe04@gmail.com>
2026-01-15 18:39:53 +0100

    Added the AutoMapper package to the project

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,16 +1,20 @@
-﻿using Contracts;
+﻿using AutoMapper;
+using Contracts;
 using Shared.DataTransferObjects;
+
 namespace Service
 {
     internal sealed class CompanyService : ICompanyService
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
 
-        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
         }
 
         public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
@@ -20,8 +24,7 @@ namespace Service
                 var companies =
              _repository.Company.GetAllCompanies(trackChanges);
 
-                var companiesDto = companies.Select(c => new CompanyDto(c.Id, c.Name ?? "", string.Join(' ', c.Address, c.Country)))
-                    .ToList();
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
 
                 return companiesDto;
             }
