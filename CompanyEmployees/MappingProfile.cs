de09783e1e61cdf4267782d230ce77eaa9a6d00e
kingaer04 <danielanifowoshe04@gmail.com>
2026-01-23 18:24:49 +0100

    Refactor CompaniesController and update mappings
    - Removed exception throw in `GetCompanies` method to allow uninterrupted retrieval of companies.
    - Updated mapping in `MappingProfile` to handle null values for `Address` and `Country`.
    - Refactored `CompanyDto` from a positional record to a standard record with nullable properties.
    - Enhanced logging in `internallog.txt` with additional debug and trace information for NLog configuration.

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -9,8 +9,9 @@ namespace CompanyEmployees
         public MappingProfile()
         {
             CreateMap<Company, CompanyDto>()
-                .ForCtorParam("FullAddress",
-                    opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+                .ForMember(c => c.FullAddress,
+                    opt => opt.MapFrom(x => string.Join(' ', x.Address ?? string.Empty, x.Country ?? string.Empty)));
+
             CreateMap<Employee, EmployeeDto>();
         }
     }
