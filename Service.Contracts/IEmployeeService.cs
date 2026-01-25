f085445bad2dda18f73c6f9970a66f780d6fca29
kingaer04 <danielanifowoshe04@gmail.com>
2026-01-25 15:28:56 +0100

    Creating a child resource

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -2,4 +2,5 @@
 {
     IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
     EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges);
+    EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employeeForCreation, bool trackChanges);
 }
