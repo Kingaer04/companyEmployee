0d85824b4e7d41074bb78bc249e0e5d09bae95b7
kingaer04 <danielanifowoshe04@gmail.com>
2026-01-27 09:43:11 +0100

    Adding the delete employee controller

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -19,5 +19,6 @@ namespace Repository
             employee.CompanyId = companyId;
             Create(employee);
         }
+        public void DeleteEmployee(Employee employee) => Delete(employee);
     }
 }
