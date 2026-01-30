9758c5e8f8c825834e7b06ce7f410e8b05e5c78b
kingaer04 <danielanifowoshe04@gmail.com>
2026-01-30 19:19:22 +0100

    Update Age property in EmployeeForCreationDto
    Changed Age from nullable int to non-nullable int with
    range validation to ensure it is at least 18 years old.

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -6,8 +6,8 @@ public record EmployeeForCreationDto
     [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
     public string? Name { get; init; }
 
-    [Required(ErrorMessage = "Age is a required field.")]
-    public int? Age { get; init; }
+    [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than 18")] 
+    public int Age { get; set; }
 
     [Required(ErrorMessage = "Position is a required field.")]
     [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")] 
