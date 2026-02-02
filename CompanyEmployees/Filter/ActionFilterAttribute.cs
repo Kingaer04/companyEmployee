cb412f93c4065387b619f0793d38f30ec3f6c77b
kingaer04 <danielanifowoshe04@gmail.com>
2026-02-02 19:07:34 +0100

    Configuring the filter

---
+++ CompanyEmployees/Filter/ActionFilterAttribute.cs
@@ -0,0 +1,4 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+public abstract class ActionFilterAttribute : Attribute, IActionFilter,
+IFilterMetadata, IAsyncActionFilter, IResultFilter, IAsyncResultFilter, IOrderedFilter 
---
+++ CompanyEmployees/Filter/ActionFilterExample.cs
@@ -0,0 +1,6 @@
+﻿namespace CompanyEmployees.Filter
+{
+    public class ActionFilterExample
+    {
+    }
+}
