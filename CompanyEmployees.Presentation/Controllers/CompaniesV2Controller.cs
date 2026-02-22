aa441c243fc95215f6b2f0bcc59e0dee941ea045
kingaer04 <danielanifowoshe04@gmail.com>
2026-02-22 14:05:34 +0100

    Implement API versioning in CompaniesV2Controller
    Updated route in CompaniesV2Controller to support API versioning.
    Modified GetCompanies method to append " V2" to company names.
    Added a comment in ServiceExtensions to indicate the status of ApiVersionReader.

--- CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesV2Controller.cs
@@ -4,7 +4,7 @@ using Asp.Versioning;
 namespace CompanyEmployees.Presentation.Controllers
 {
     [ApiVersion("2.0")]
-    [Route("api/companies")]
+    [Route("api/{v:apiversion}/companies")]
     [ApiController]
     public class CompaniesV2Controller : ControllerBase
     {
@@ -18,7 +18,9 @@ namespace CompanyEmployees.Presentation.Controllers
             var companies = await _services.CompanyService
                 .GetAllCompaniesAsync(trackChanges: false);
 
-            return Ok(companies);
+            var companiesV2 = companies.Select(x => $"{x.Name} V2");
+
+            return Ok(companiesV2);
         }
     }
 }
--- CompanyEmployees/Extensions/ServiceExtentions.cs
+++ CompanyEmployees/Extensions/ServiceExtentions.cs
@@ -69,6 +69,7 @@ namespace CompanyEmployees.Extensions
                 opt.ReportApiVersions = true;
                 opt.AssumeDefaultVersionWhenUnspecified = true;
                 opt.DefaultApiVersion = new ApiVersion(1, 0);
+                //opt.ApiVersionReader = new HeaderApiVersionReader("api-version");
             }).AddMvc();
         }
     }
