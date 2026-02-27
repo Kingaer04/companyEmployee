e8a30eeeb1be708414f344172237089221f08c96
kingaer04 <danielanifowoshe04@gmail.com>
2026-02-27 17:37:44 +0100

    Add authentication service to IServiceManager
    Updated IServiceManager to include AuthenticationService for enhanced authentication capabilities. Modified ServiceManager to support lazy loading of IAuthenticationService, added necessary using directives, and updated the constructor to accept UserManager and IConfiguration parameters.

--- Service.Contracts/IServiceManager.cs
+++ Service.Contracts/IServiceManager.cs
@@ -1,5 +1,8 @@
-﻿public interface IServiceManager
+﻿using Service.Contracts;
+
+public interface IServiceManager
 {
     ICompanyService CompanyService { get; }  // ← PROMISE: "I will have this property"
     IEmployeeService EmployeeService { get; }
+    IAuthenticationService AuthenticationService { get; }
 }
