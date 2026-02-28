c338ae9a874474cc6e8a896e635c70a65d757e53
kingaer04 <danielanifowoshe04@gmail.com>
2026-02-28 12:55:17 +0100

    Update authorization in CompaniesController and log config
    Changed authorization for GetCompanies to require "Manager" role. Simplified constructor syntax. Updated internallog.txt with detailed NLog configuration debug messages.

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -18,7 +18,7 @@ namespace CompanyEmployees.Presentation.Controllers
         public CompaniesController(IServiceManager service) => _service = service;
 
         [HttpGet(Name = "GetCompanies")]
-        [Authorize]
+        [Authorize(Roles = "Manager")]
         public async Task<IActionResult> GetCompanies()
         {
             var companies = await
