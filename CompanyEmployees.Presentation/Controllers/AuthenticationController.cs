d81641541be3c96c5fda2bcc6221d88a6ec43175
kingaer04 <danielanifowoshe04@gmail.com>
2026-03-03 23:12:11 +0100

    Enhance token creation in AuthenticationController
    Updated the `Authenticate` method to generate tokens with expiration details by setting the `populateExp` parameter to `true` in the `CreateToken` method. This improves security and session management by providing necessary expiration information in the authentication tokens.

--- CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -32,11 +32,11 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (!await _service.AuthenticationService.ValidateUser(user))
                 return Unauthorized();
-            return Ok(new
-            {
-                Token = await _service
-            .AuthenticationService.CreateToken()
-            });
+
+            var tokenDto = await _service.AuthenticationService
+                .CreateToken(populateExp: true);
+
+            return Ok(tokenDto);
         }
     }
 }
