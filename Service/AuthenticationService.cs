19b5626a6f2adce180da311686b0196f8c8e8f71
kingaer04 <danielanifowoshe04@gmail.com>
2026-03-03 23:52:22 +0100

    Added TokenController

--- Service/AuthenticationService.cs
+++ Service/AuthenticationService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
@@ -140,6 +141,17 @@ namespace Service
             }
 
             return principal;
+        }
+        public async Task<TokenDto> RefreshToken(TokenDto tokenDto)
+        {
+            var principal = GetPrincipalFromExpiredToken(tokenDto.AccessToken);
+
+            var user = await _userManager.FindByNameAsync(principal.Identity.Name);
+            if (user == null || user.RefreshToken != tokenDto.RefreshToken || user.RefreshTokenExpiraryTime <= DateTime.Now)
+                throw new RefreshTokenBadRequest();
+            _user = user;
+
+            return await CreateToken(populateExp: false);
         }
     }
 }
