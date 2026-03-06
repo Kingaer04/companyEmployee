873224bbde98be42a468b23fea5289e91e570ccd
kingaer04 <danielanifowoshe04@gmail.com>
2026-03-06 19:16:12 +0100

    Add AddJwtConfiguration extension method
    This commit introduces a new extension method `AddJwtConfiguration` to the `IServiceCollection` interface in the `CompanyEmployees.Extensions` namespace. The method binds the `JwtSettings` section from the provided `IConfiguration` instance to a `JwtConfiguration` object, facilitating JWT configuration in the application.

--- CompanyEmployees/Extensions/ServiceExtentions.cs
+++ CompanyEmployees/Extensions/ServiceExtentions.cs
@@ -150,5 +150,7 @@ namespace CompanyEmployees.Extensions
                 };
             });
         }
+        public static void AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration) =>
+            services.Configure<JwtConfiguration>(configuration.GetSection("JwtSettings"));
     }
 }
